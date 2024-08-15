f551a70781f1e50bf0a59fd8683ab68544b17eef
Mohamed Sayed <ms7237322@gmail.com>
2024-08-15 17:34:13 +0300

    New Version

--- src/app/components/PayPalButton.jsx
+++ src/app/components/PayPalButton.jsx
@@ -49,7 +49,7 @@ export default function PayPalCheckout({ selectedPackage }) {
 
   return (
     <PayPalScriptProvider options={paypalOptions}>
-      <div className="paypal-button-container   ">
+      <div className="paypal-button-container  w-96 p-7 pb-0 rounded-3xl border border-white shadow-inner shadow-[black]  bg-white">
         <PayPalButtons
           style={{ layout: "vertical" }}
           createOrder={createOrder}
