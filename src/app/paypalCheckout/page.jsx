8e86bfb4739e373324bf5a7f9e59906a4352c7d3
Mohamed Sayed <ms7237322@gmail.com>
2024-09-21 20:08:33 +0300

    about page

---
+++ src/app/paypalCheckout/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+
+function page() {
+  const pathname = usePathname();
+  const router = useRouter(); // Using the correct useRouter hook
+  const [packageName, setPackageName] = useState("");
+  return (
+    <div className=" h-[100vh] font-bold text-white text-4xl">
+      <h1>{pathname}</h1>
+    </div>
+  );
+}
+
+export default page;
