f4b937bfcb766d9e9ccbff0b8eb040eb106f8f50
Mohamed Sayed <ms7237322@gmail.com>
2024-11-12 08:38:26 +0200

    optimize  performance

--- src/app/components/HeroSection/HeroSection.jsx
+++ src/app/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Link from "next/link";
 import PackageButton from "../PackageButton/PackageButton";
 import Photo from "../Photo";
 import Social from "../Social";
