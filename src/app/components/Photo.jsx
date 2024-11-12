c158777668779972c864ae98c1254795ae6a0163
Mohamed Sayed <ms7237322@gmail.com>
2024-11-12 07:28:23 +0200

    ptimize  performance

--- src/app/components/Photo.jsx
+++ src/app/components/Photo.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Image from "next/image";
-import girlImg from "../../../public/heroSectionPhotoEdited - Copy.png"
+import girlImg from "../../../public/heroSectionPhotoEdited - Copy.png";
 function Photo() {
   return (
     <div className="w-full h-full relative">
@@ -26,7 +26,8 @@ function Photo() {
             // src="/heroSectionPhotoEdited.png "
             src={girlImg}
             priority
-            quality={100}
+            loading="eager"
+            quality={85}
             fill
             alt="heroSectionPhoto"
             className="object-contain"
@@ -35,7 +36,7 @@ function Photo() {
         {/* circle */}
         <motion.svg
           className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]"
-          initial={{ y: -10,x:0 }}
+          initial={{ y: -10, x: 0 }}
           fill="transparent"
           viewBox="0 0 506 506"
           xmlns="http://www.w3.org/2000/svg"
@@ -48,12 +49,21 @@ function Photo() {
             strokeWidth="4"
             strokeLinecap="round"
             strokeLinejoin="round"
-            initial={{strokeDasharray: "24 10 0 0"}}
-            animate={{strokeDasharray: ["15 120 25 25", "16 25 92 72", "16 25 92 72", "4 250 22 22"],rotate: [120, 360],}}
-            transition={{
-                duration:20, repeat: Infinity, repeatType:"reverse"
+            initial={{ strokeDasharray: "24 10 0 0" }}
+            animate={{
+              strokeDasharray: [
+                "15 120 25 25",
+                "16 25 92 72",
+                "16 25 92 72",
+                "4 250 22 22",
+              ],
+              rotate: [120, 360],
             }}
-            
+            transition={{
+              duration: 20,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
           />
         </motion.svg>
       </motion.div>
