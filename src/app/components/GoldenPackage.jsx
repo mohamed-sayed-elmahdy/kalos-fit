67454352b56c2dd1944c6502566952653ac119a7
Mohamed Sayed <ms7237322@gmail.com>
2024-12-03 17:40:14 +0200

    path

--- src/app/components/GoldenPackage.jsx
+++ src/app/components/GoldenPackage.jsx
@@ -4,15 +4,13 @@ import CounterdownTimer from "./CountdownTimer";
 import Link from "next/link";
 
 function GoldenPackage() {
-
   return (
-    <div className="p-2 flex justify-center items-center">
+    <div className={`p-2 flex justify-center items-center`}>
       <div
-        className={`relative text-xl rounded-3xl border p-2 max-w-[390px] flex flex-col justify-between overflow-hidden "lg:transform xl:scale-110 z-10" : ""
-         border-[#eeba2b]`}
+        className={`relative text-xl rounded-3xl border p-2 max-w-[390px] flex flex-col justify-between overflow-hidden lg:transform xl:scale-110 z-10 border-[#eeba2b]`}
       >
         <div
-          className=" mt-2 mb-3 bg-gold text-black text-2xl font-extrabold py-2 text-center rounded-3xl"
+          className="mt-2 mb-3 bg-gold text-black text-2xl font-extrabold py-2 text-center rounded-3xl"
           style={{ backgroundColor: "#eeba2b" }}
         >
           Golden Offer
@@ -20,18 +18,16 @@ function GoldenPackage() {
 
         {/* Timer and One-Day Offer */}
         <div className="flex items-center justify-evenly gap-2 mt-4 mb-4 text-gold text-sm flex-wrap">
-          {/* النص */}
           <span className="text-base md:text-xl font-semibold">
             For Just <span className="text-[#eeba2b]">One Day</span>
           </span>
-          {/* تايمر */}
           <CounterdownTimer
             startDate="2024-12-03T00:00:00"
             endDate="2024-12-04T00:00:00"
           />
         </div>
 
-        {/* ضمان الثلاثة أشهر */}
+        {/* 3-Month Guarantee */}
         <div className="text-center my-2 font-semibold text-lg md:text-2xl">
           <div className="text-white ">
             If you didn't get an incredible transformation in just 3 months
@@ -41,7 +37,7 @@ function GoldenPackage() {
           </div>
         </div>
 
-        {/* الصور */}
+        {/* Images */}
         <div className="grid grid-cols-2 gap-4 mb-6 ">
           <img
             src="/feedback1.jpg"
@@ -49,20 +45,19 @@ function GoldenPackage() {
             className="w-full rounded-lg"
           />
           <img
-              src="/feedback2.jpg"
+            src="/feedback2.jpg"
             alt="Image 2"
             className="w-full rounded-lg md:h-[184.1px]"
-            
           />
         </div>
 
-        {/* الزر */}
+        {/* Button */}
         <Link href={"/packages"} className="flex justify-center items-center">
-        <button
-          className="self-center w-60 text-black py-2 rounded-full text-xl transition-all mt-auto bg-[#eeba2b] hover:bg-[#ffd562]"
-        >
-          Get It Now
-        </button>
+          <button
+            className="self-center w-60 text-black py-2 rounded-full text-xl transition-all mt-auto bg-[#eeba2b] hover:bg-[#ffd562]"
+          >
+            Get It Now
+          </button>
         </Link>
       </div>
     </div>
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,32 +1,38 @@
-// app/layout.jsx
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
-import { League_Spartan, Playfair_Display } from 'next/font/google';
-import Script from 'next/script';
-import './globals.css';
-import NavBar from './components/Navbar/Navbar';
-import Footer from './components/Footer/Footer';
-import { GA_TRACKING_ID } from './lib/gtag'; // Import GA_TRACKING_ID
-const leagueSpartan = League_Spartan({ subsets: ['latin'],display:"swap" });
+
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
+import { League_Spartan, Playfair_Display } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
+import NavBar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import { GA_TRACKING_ID } from "./lib/gtag"; // Import GA_TRACKING_ID
+const leagueSpartan = League_Spartan({ subsets: ["latin"], display: "swap" });
+ const playfairDisplay = Playfair_Display({
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export const metadata = {
-  title: 'Kalos Fit',
+  title: "Kalos Fit",
   description:
-    'Your empowering partner in health and wellness! We understand the unique challenges you face when it comes to nutrition. Between work, family, and daily commitments, it\'s easy to feel overwhelmed and fall behind on your health goals. That\'s why we create personalized nutrition plans designed for your lifestyle.',
+    "Your empowering partner in health and wellness! We understand the unique challenges you face when it comes to nutrition. Between work, family, and daily commitments, it's easy to feel overwhelmed and fall behind on your health goals. That's why we create personalized nutrition plans designed for your lifestyle.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${leagueSpartan.className}`}>
+      <body
+        className={`${leagueSpartan.className} ${playfairDisplay.className}`}
+      >
         {/* Google Analytics Scripts */}
         <Script
-        async
+          async
           strategy="lazyOnload"
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} // Use GA_TRACKING_ID
         />
         <Script
-        async
+          async
           id="google-analytics"
           strategy="lazyOnload"
           dangerouslySetInnerHTML={{
@@ -40,7 +46,7 @@ export default function RootLayout({ children }) {
             `,
           }}
         />
-        
+
         <NavBar />
         {children}
         <Analytics />
